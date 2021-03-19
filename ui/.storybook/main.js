8f82dea14ca2f2b9b6c65e2d400835ef02232c76
Julien Giovaresco <dev@giovaresco.fr>
2021-03-19 08:10:34 +0100

    👷 configure storybook

---
+++ ui/.storybook/main.js
@@ -0,0 +1,34 @@
+const snowpackConfig = require('../snowpack.config');
+const webpack = require('webpack');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config) => {
+    // Assign aliases from snowpack.config.js
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...snowpackConfig.alias,
+    };
+    // Add rules for supporting import.meta
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        require.resolve('@open-wc/webpack-import-meta-loader'),
+        require.resolve(
+          '@snowpack/plugin-webpack/plugins/proxy-import-resolve',
+        ),
+      ],
+    });
+    // Add __SNOWPACK_ENV__ global
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __SNOWPACK_ENV__: JSON.stringify(process.env),
+      }),
+    );
+    return config;
+  },
+};
