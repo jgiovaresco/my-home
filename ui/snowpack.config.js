44bd757514fc575bed6120d14baa03aa3d736b02
Julien Giovaresco <dev@giovaresco.fr>
2021-03-22 08:00:52 +0100

    🔨 configure sass

--- ui/snowpack.config.js
+++ ui/snowpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     src: { url: '/dist' },
   },
   plugins: [
+    '@snowpack/plugin-sass',
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
