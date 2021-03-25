e9cd8aecf62424e5157e431eb3e210494d22e170
Julien Giovaresco <dev@giovaresco.fr>
2021-03-25 19:12:12 +0100

    🎨 use Header component

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home } from './pages';
+import { Header } from './core/components';
 
 const routes = [
   {
     path: '/',
+    label: 'Home',
     component: Home,
   },
 ];
 
 export function App() {
+  const navItems = routes.map(({ path, label }) => ({ path, label }));
+
   return (
     <Router>
-      <div>
-        <Switch>
-          {routes.map((route) => (
-            <Route exact key={route.path} path={route.path}>
-              <route.component />
-            </Route>
-          ))}
-        </Switch>
-      </div>
+      <Header navItems={navItems} />
+
+      <Switch>
+        {routes.map(({ path, component: Page }) => (
+          <Route exact key={path} path={path} render={() => <Page />} />
+        ))}
+      </Switch>
     </Router>
   );
 }
