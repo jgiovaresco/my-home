795b3ea34125b8e530e0b74674cb254cac2af0fe
Julien Giovaresco <dev@giovaresco.fr>
2021-10-16 16:50:45 +0200

    💄 customize Home page
    https://linear.app/jgi/issue/JGI-12

--- ui/src/home/HomeRoute.tsx
+++ ui/src/home/HomeRoute.tsx
@@ -1,37 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './HomeRoute.scss';
-import Logo from './logo.svg';
+import Logo from './house.svg';
 
 export function HomeRoute() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
   return (
     <div className="App">
-      <header className="App-header">
-        <Logo />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <Logo className="logo" />
+      <p>Welcome to MyHome!</p>
     </div>
   );
 }
